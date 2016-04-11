27e36feb23ae1b689dd568887b36b899745a346c
Anton Haughey <haughey@dsv.su.se>
2016-04-11 13:29:59 +0200

    Added 'Reduction of candidates'-criterions to the 'Reduction of candidates'-page.

--- designer/src/main/java/eu/esens/espdvcd/designer/components/ESPDRequestForm.java
+++ designer/src/main/java/eu/esens/espdvcd/designer/components/ESPDRequestForm.java
@@ -20,10 +20,13 @@ public class ESPDRequestForm extends ESPDForm {
 
     private CheckBox selectAllExclusionCriteriaCheckbox = new CheckBox("Select all exclusion criteria");
     private CheckBox selectAllSelectionCriteriaCheckbox = new CheckBox("Select all selection criteria");
+    private CheckBox selectAllReductionCriteriaCheckbox = new CheckBox("Select all selection criteria");
     private HashMap<String,List<CriterionForm>> exclusionCriterionHash = new HashMap<String,List<CriterionForm>>();
     private HashMap<String,List<CriterionForm>> selectionCriterionHash = new HashMap<String,List<CriterionForm>>();
+    private HashMap<String,List<CriterionForm>> reductionCriterionHash = new HashMap<String,List<CriterionForm>>();
     private List<CriterionGroupForm> exclusionCriterionGroupForms = new ArrayList<>();
     private List<CriterionGroupForm> selectionCriterionGroupForms = new ArrayList<>();
+    private List<CriterionGroupForm> reductionCriterionGroupForms = new ArrayList<>();
 
     public ESPDRequestForm(Master view, ESPDRequest espdRequest) {
         super(view, espdRequest);
@@ -115,6 +118,41 @@ public class ESPDRequestForm extends ESPDForm {
         // Page 4 - Reduction of candidates
         VerticalLayout page4 = newPage("Reduction of candidates", "Reduction of candidates");
 
+        VerticalLayout reductionActionLayout = new VerticalLayout();
+        reductionActionLayout.setMargin(true);
+        reductionActionLayout.addComponent(selectAllReductionCriteriaCheckbox);
+        selectAllReductionCriteriaCheckbox.setValue(true);
+        page4.addComponent(reductionActionLayout);
+
+        selectAllReductionCriteriaCheckbox.addValueChangeListener(this::onSelectAllReductionCriteria);
+        selectAllReductionCriteriaCheckbox.setStyleName("espdRequestForm-checkbox");
+
+        for (SelectableCriterion criterion : espdRequest.getReductionOfCandidatesCriteriaList()) {
+
+            if (!reductionCriterionHash.containsKey(criterion.getTypeCode())) {
+                reductionCriterionHash.put(criterion.getTypeCode(), new ArrayList<CriterionForm>());
+            }
+
+            CriterionForm criterionForm = new CriterionForm(view, criterion);
+            reductionCriterionHash.get(criterion.getTypeCode()).add(criterionForm);
+        }
+
+        for (Map.Entry<String, List<CriterionForm>> entry : reductionCriterionHash.entrySet()) {
+            String key = entry.getKey();
+            List<CriterionForm> criterionForms = entry.getValue();
+
+            String fullTypeCodeName = Codelists.CriteriaType.getValueForId(key);
+
+            if (fullTypeCodeName == null) {
+                // Fallback to display the type code instead of real name
+                fullTypeCodeName = key;
+            }
+
+            CriterionGroupForm criterionGroupForm = new CriterionGroupForm(fullTypeCodeName, criterionForms);
+            reductionCriterionGroupForms.add(criterionGroupForm);
+            page4.addComponent(new CriterionGroupForm(fullTypeCodeName, criterionForms));
+        }
+
         // Page 5 - Finish
         VerticalLayout page5 = newPage("Finish", "Finish");
     }
@@ -140,6 +178,18 @@ public class ESPDRequestForm extends ESPDForm {
     public void onSelectAllSelectionCriteria(Property.ValueChangeEvent event) {
         for (CriterionGroupForm criterionGroupForm : selectionCriterionGroupForms) {
             criterionGroupForm.setSelectedOnAllCriteria(selectAllSelectionCriteriaCheckbox.getValue());
+        }
+    }
+
+    /**
+     * Selects or deselects all reduction criteria depending on
+     * the boolean value of the selectAllReductionCriteria checkbox
+     *
+     * @param event Vaadin7 value change event
+     */
+    public void onSelectAllReductionCriteria(Property.ValueChangeEvent event) {
+        for (CriterionGroupForm criterionGroupForm : reductionCriterionGroupForms) {
+            criterionGroupForm.setSelectedOnAllCriteria(selectAllReductionCriteriaCheckbox.getValue());
         }
     }
 }
