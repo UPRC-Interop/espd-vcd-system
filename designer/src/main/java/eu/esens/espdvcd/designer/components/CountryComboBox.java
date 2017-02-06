56100496043a9f069a956ebaa718f885d25a5ce8
Jerry Dimitriou <jerouris@unipi.gr>
2017-02-06 13:00:38 +0200

    Fixed CountryComboBox issue with new vaadin

--- designer/src/main/java/eu/esens/espdvcd/designer/components/CountryComboBox.java
+++ designer/src/main/java/eu/esens/espdvcd/designer/components/CountryComboBox.java
@@ -6,11 +6,8 @@ package eu.esens.espdvcd.designer.components;
 
 import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.ComboBox;
-import com.vaadin.ui.UI;
 import eu.esens.espdvcd.codelist.Codelists;
 import org.apache.commons.lang3.text.WordUtils;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.*;
 
 public class CountryComboBox extends ComboBox {
@@ -24,17 +21,11 @@ public class CountryComboBox extends ComboBox {
             String countryId = Codelists.CountryIdentification.getIdForData(countryName);
             addItem(countryId);
             setItemCaption(countryId, WordUtils.capitalize(countryName.toLowerCase()));
-            String flagPath = "img/flags_iso/24/" + countryId.toLowerCase() + ".png";
 
-            UI ui = UI.getCurrent();
-            try (InputStream s = ui.getSession().getService().getThemeResourceAsStream(ui, ui.getTheme(), flagPath)) {
-                ThemeResource iconResource = new ThemeResource("img/flags_iso/24/" + countryId.toLowerCase() + ".png");
-                setItemIcon(countryId, iconResource);
-            } catch (IOException ex) {
-                ThemeResource iconResource = new ThemeResource("img/flags_iso/24/null.png");
-                setItemIcon(countryId, iconResource);
-            }
-            
+            ThemeResource iconResource = new ThemeResource("img/flags_iso/24/" + countryId.toLowerCase() + ".png");
+            setItemIcon(countryId, iconResource);
+            //    ThemeResource iconResource = new ThemeResource("img/flags_iso/24/null.png");
+            //    setItemIcon(countryId, iconResource);
         }
 
         this.setInputPrompt("Select country");
