aaa12f12e5d905440f0c9b76081dbf5562d057fe
konstantinos <konstantinos_raptis@hotmail.com>
2017-09-13 14:43:42 +0300

    Custom bindings approach replaced with gradle dependency injection

--- schema/src/test/java/eu/esens/espdvcd/schema/ESPDCreationTests.java
+++ schema/src/test/java/eu/esens/espdvcd/schema/ESPDCreationTests.java
@@ -1,37 +1,46 @@
 package eu.esens.espdvcd.schema;
 
-import eu.espd.schema.v1.espdrequest_1.ESPDRequestType;
-import eu.espd.schema.v1.espdrequest_1.ObjectFactory;
-import eu.espd.schema.v1.espdresponse_1.ESPDResponseType;
-
-import eu.espd.schema.v1.commonaggregatecomponents_2.AddressType;
-import eu.espd.schema.v1.commonaggregatecomponents_2.ContractingPartyType;
-import eu.espd.schema.v1.commonaggregatecomponents_2.PartyType;
-import eu.espd.schema.v1.commonbasiccomponents_2.BuildingNameType;
-import eu.espd.schema.v1.commonbasiccomponents_2.IDType;
-
+//import eu.espd.schema.v1.espdrequest_1.ESPDRequestType;
+//import eu.espd.schema.v1.espdrequest_1.ObjectFactory;
+//import eu.espd.schema.v1.espdresponse_1.ESPDResponseType;
+//
+//import eu.espd.schema.v1.commonaggregatecomponents_2.AddressType;
+//import eu.espd.schema.v1.commonaggregatecomponents_2.ContractingPartyType;
+//import eu.espd.schema.v1.commonaggregatecomponents_2.PartyType;
+//import eu.espd.schema.v1.commonbasiccomponents_2.BuildingNameType;
+//import eu.espd.schema.v1.commonbasiccomponents_2.IDType;
+import grow.names.specification.ubl.schema.xsd.espdrequest_1.ESPDRequestType;
+import grow.names.specification.ubl.schema.xsd.espdrequest_1.ObjectFactory;
+import grow.names.specification.ubl.schema.xsd.espdresponse_1.ESPDResponseType;
 import java.io.ByteArrayOutputStream;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Marshaller;
+import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_2.AddressType;
+import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_2.ContractingPartyType;
+import oasis.names.specification.ubl.schema.xsd.commonaggregatecomponents_2.PartyType;
+import oasis.names.specification.ubl.schema.xsd.commonbasiccomponents_2.BuildingNameType;
+import oasis.names.specification.ubl.schema.xsd.commonbasiccomponents_2.IDType;
 
 import org.junit.Before;
 import org.junit.Test;
+import test.x.ubl.pre_award.qualificationapplicationrequest.QualificationApplicationRequestType;
+import test.x.ubl.pre_award.qualificationapplicationresponse.QualificationApplicationResponseType;
 
 /**
  *
  */
 public class ESPDCreationTests {
-    
+
     public ESPDCreationTests() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @Test
     public void createESPDRequest() {
-        
+
         ObjectFactory of = new ObjectFactory();
         ESPDRequestType req = new ESPDRequestType();
 
@@ -39,7 +48,7 @@ public class ESPDCreationTests {
         req.setID(new IDType());
         req.getID().setSchemeID("Lala");
         req.getID().setValue("Value");
-        
+
         req.setContractingParty(new ContractingPartyType());
         req.getContractingParty().setParty(new PartyType());
         req.getContractingParty().getParty().setPostalAddress(new AddressType());
@@ -48,38 +57,100 @@ public class ESPDCreationTests {
 
         //This must be validated using code list for examle
         req.getContractingParty().getParty().getPostalAddress().getBuildingName().setLanguageID("EN");
-        
-        System.out.println(toXml(of.createESPDRequest(req)));
-        
+
+        System.out.println(toXmlForV1(of.createESPDRequest(req)));
+
     }
-    
+
     @Test
     public void createESPDResponse() {
 
-        eu.espd.schema.v1.espdresponse_1.ObjectFactory of = new eu.espd.schema.v1.espdresponse_1.ObjectFactory();
+        // eu.espd.schema.v1.espdresponse_1.ObjectFactory of = new eu.espd.schema.v1.espdresponse_1.ObjectFactory();
+        grow.names.specification.ubl.schema.xsd.espdresponse_1.ObjectFactory of = new grow.names.specification.ubl.schema.xsd.espdresponse_1.ObjectFactory();
         ESPDResponseType res = new ESPDResponseType();
         res.setID(new IDType());
         res.getID().setSchemeID("Lala");
         res.getID().setValue("Value");
-        
-        System.out.println(toXml(of.createESPDResponse(res)));
-        
+
+        System.out.println(toXmlForV1(of.createESPDResponse(res)));
+
     }
-    
-    private String toXml(JAXBElement element) {
-    try {
 
-        Marshaller marshaller = SchemaUtil.getMarshaller();
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);  
-        
+    @Test
+    public void createESPDRequestForV2() {
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        marshaller.marshal(element, baos);
-        return baos.toString();
-    } catch (Exception e) {
-        e.printStackTrace();
-    }      
-    return "";
-}
+        test.x.ubl.pre_award.qualificationapplicationrequest.ObjectFactory of = new test.x.ubl.pre_award.qualificationapplicationrequest.ObjectFactory();
+        QualificationApplicationRequestType req = new QualificationApplicationRequestType();
+
+        //Add an ID
+        req.setID(new test.x.ubl.pre_award.commonbasic.IDType());
+        req.getID().setSchemeID("Lala");
+        req.getID().setValue("Value");
+
+        req.getContractingParty().add(new test.x.ubl.pre_award.commonaggregate.ContractingPartyType());
+        // req.setContractingParty(new ContractingPartyType());
+
+        req.getContractingParty().get(0).setParty(new test.x.ubl.pre_award.commonaggregate.PartyType());
+        // req.getContractingParty().setParty(new PartyType());
+
+        req.getContractingParty().get(0).getParty().setPostalAddress(new test.x.ubl.pre_award.commonaggregate.AddressType());
+        // req.getContractingParty().getParty().setPostalAddress(new AddressType());
+
+        req.getContractingParty().get(0).getParty().getPostalAddress().setBuildingName(new test.x.ubl.pre_award.commonbasic.BuildingNameType());
+        // req.getContractingParty().getParty().getPostalAddress().setBuildingName(new BuildingNameType());
+
+        req.getContractingParty().get(0).getParty().getPostalAddress().getBuildingName().setValue("Address Building 123");
+        // req.getContractingParty().getParty().getPostalAddress().getBuildingName().setValue("Address Building 123");
+
+        //This must be validated using code list for examle
+        req.getContractingParty().get(0).getParty().getPostalAddress().getBuildingName().setLanguageID("EN");
+        //req.getContractingParty().getParty().getPostalAddress().getBuildingName().setLanguageID("EN");
+
+        System.out.println(toXmlForV2(of.createQualificationApplicationRequest(req)));
+
+    }
+
+    @Test
+    public void createESPDResponseForV2() {
+
+        test.x.ubl.pre_award.qualificationapplicationresponse.ObjectFactory of = new test.x.ubl.pre_award.qualificationapplicationresponse.ObjectFactory();
+        QualificationApplicationResponseType res = new QualificationApplicationResponseType();
+        res.setID(new test.x.ubl.pre_award.commonbasic.IDType());
+        res.getID().setSchemeID("Lala");
+        res.getID().setValue("Value");
+
+        System.out.println(toXmlForV2(of.createQualificationApplicationResponse(res)));
+
+    }
+
+    private String toXmlForV1(JAXBElement element) {
+        try {
+
+            Marshaller marshaller = SchemaUtil.getMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            marshaller.marshal(element, baos);
+            return baos.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private String toXmlForV2(JAXBElement element) {
+        try {
+
+            Marshaller marshaller = SchemaUtil.getMarashallerForV2();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            marshaller.marshal(element, baos);
+            return baos.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
     
 }
