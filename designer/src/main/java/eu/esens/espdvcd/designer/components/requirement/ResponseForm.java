cfbd3c8711ae5b3f6731a46015edd688a2c2c03a
Maria Siapera <mariaspr@unipi.gr>
2018-03-06 16:18:04 +0200

    -Removed old designer project.-Added new folder structure

--- designer/src/main/java/eu/esens/espdvcd/designer/components/requirement/ResponseForm.java
+++
@@ -1,9 +0,0 @@
-package eu.esens.espdvcd.designer.components.requirement;
-
-import com.vaadin.ui.FormLayout;
-
-public class ResponseForm extends FormLayout {
-    public ResponseForm() {
-        setStyleName("ResponseForm");
-    }
-}
