679ce2691ca1afd1e7e48acc27e172d73a86fb9d
Maria Siapera <mariaspr@unipi.gr>
2018-04-04 15:05:29 +0300

    -Fixed requirement and requirement-group

--- designer/designer-frontend/src/app/requirement-group/requirement-group.component.ts
+++ designer/designer-frontend/src/app/requirement-group/requirement-group.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {RequirementGroup} from '../model/requirementGroup.model';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-requirement-group',
   templateUrl: './requirement-group.component.html',
   styleUrls: ['./requirement-group.component.css']
 })
-export class RequirementGroupComponent implements OnInit {
+export class RequirementGroupComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() reqGroup: RequirementGroup;
+  @Input() form: FormGroup;
+  @Input() indicator: boolean;
+  public childIndicator: boolean;
+
+  public showIndicator: boolean;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  ngOnChanges() {
+    this.showIndicator = this.checkIndicator(this.indicator);
+    console.log('Reg Group Id: ' + this.reqGroup.id + ' show Indicator is: ' + this.showIndicator);
+  }
+
+  checkIndicator(b: boolean): boolean {
+
+    console.log('Check Indicator called ' + this.reqGroup.id + ' with value of indicator: ' + this.indicator);
+    if (this.reqGroup.condition) {
+      if (this.reqGroup.condition.endsWith('ON_FALSE')) {
+        return !b;
+      } else {
+        return b;
+      }
+    }
+    return true;
+  }
+
+  childIndicatorChangedHandler(event: string) {
+    this.childIndicator = (event === 'YES');
+  }
 }
--- designer/designer-frontend/src/app/requirement/requirement.component.ts
+++ designer/designer-frontend/src/app/requirement/requirement.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, Output, OnInit, EventEmitter, OnChanges} from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {Requirement} from '../model/requirement.model';
 
 @Component({
   selector: 'app-requirement',
   templateUrl: './requirement.component.html',
   styleUrls: ['./requirement.component.css']
 })
-export class RequirementComponent implements OnInit {
+export class RequirementComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() req: Requirement;
+  @Input() form: FormGroup;
+
+  @Output() indicatorChanged = new EventEmitter();
+
+  constructor() {
+  }
+
+  ngOnChanges() {
+    this.indicatorChanged.emit(this.form.get(this.req.id).value);
+  }
 
   ngOnInit() {
+    if (this.req.responseDataType === 'INDICATOR') {
+      this.form.get(this.req.id)
+        .valueChanges
+        .subscribe(ev => {
+          console.log('emit: ' + ev);
+          this.indicatorChanged.emit(ev);
+        });
+    }
   }
+
 
 }
