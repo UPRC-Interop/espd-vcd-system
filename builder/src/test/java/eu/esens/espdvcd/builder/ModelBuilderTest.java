c55b0072b0c4155191cd4a1dc54c1f308f3e97ab
konstantinos <konstantinos_raptis@hotmail.com>
2018-06-02 02:30:24 +0300

    -findArtefactVersion bug fixed -findSchemaVersion improved -2.0.1 enum value added in ProfileExecutionIDEnum

--- builder/src/test/java/eu/esens/espdvcd/builder/ModelBuilderTest.java
+++ builder/src/test/java/eu/esens/espdvcd/builder/ModelBuilderTest.java
@@ -12,7 +12,7 @@ import java.io.InputStream;
 public class ModelBuilderTest implements ModelBuilder {
 
     private InputStream espdRequestRegulatedV1_0_2 = ModelBuilderTest.class.getResourceAsStream("/espd-request.xml");
-    private InputStream espdRequestRegulatedV2_0_2 = ModelBuilderTest.class.getResourceAsStream("/espd-request-v2_2018-05-30a.xml");
+    private InputStream espdRequestRegulatedV2_0_1 = ModelBuilderTest.class.getResourceAsStream("/espd-request-v2_2018-05-30a.xml");
 
     private InputStream espdResponseRegulatedV1_0_2 = ModelBuilderTest.class.getResourceAsStream("/espd-response.xml");
     private InputStream espdResponseRegulatedV2_0_2 = ModelBuilderTest.class.getResourceAsStream("/REGULATED_ESPD-Response_2.0.2.xml");
@@ -25,7 +25,7 @@ public class ModelBuilderTest implements ModelBuilder {
     @Ignore
     @Test
     public void testFindArtefactVersion() throws BuilderException {
-        Assert.assertEquals(ProfileExecutionIDEnum.ESPD_EDM_V2_0_2_REGULATED, findEDMArtefactVersion(espdRequestRegulatedV2_0_2));
+        Assert.assertEquals(ProfileExecutionIDEnum.ESPD_EDM_V2_0_1_REGULATED, findEDMArtefactVersion(espdRequestRegulatedV2_0_1));
         Assert.assertNotEquals(ProfileExecutionIDEnum.ESPD_EDM_V2_0_2_REGULATED, findEDMArtefactVersion(espdRequestRegulatedV1_0_2));
         Assert.assertNotEquals(ProfileExecutionIDEnum.ESPD_EDM_V2_0_2_REGULATED, findEDMArtefactVersion(espdRequestRegulatedV1_0_2));
         Assert.assertEquals(ProfileExecutionIDEnum.ESPD_EDM_V1_0_2, findEDMArtefactVersion(espdRequestRegulatedV1_0_2));
--- codelists/src/main/java/eu/esens/espdvcd/codelist/enums/ProfileExecutionIDEnum.java
+++ codelists/src/main/java/eu/esens/espdvcd/codelist/enums/ProfileExecutionIDEnum.java
@@ -9,10 +9,16 @@ package eu.esens.espdvcd.codelist.enums;
  */
 public enum ProfileExecutionIDEnum {
 
+    // 2.0.0
     ESPD_EDM_V2_0_0_REGULATED("ESPD-EDMv2.0.0-REGULATED"),
     ESPD_EDM_V2_0_0_SELFCONTAINED("ESPD-EDMv2.0.0-SELFCONTAINED"),
+    // 2.0.1
+    ESPD_EDM_V2_0_1_REGULATED("ESPD-EDMv2.0.1-REGULATED"),
+    ESPD_EDM_V2_0_1_SELFCONTAINED("ESPD-EDMv2.0.1-SELFCONTAINED"),
+    // 2.0.2
     ESPD_EDM_V2_0_2_REGULATED("ESPD-EDMv2.0.2-REGULATED"),
     ESPD_EDM_V2_0_2_SELFCONTAINED("ESPD-EDMv2.0.2-SELFCONTAINED"),
+    // 1.0.2
     ESPD_EDM_V1_0_2("ESPD-EDMv1.0.2"),
     // unknown enum value is not part of the relevant codelist
     UNKNOWN("UNKNOWN");
