aa2394c892f6c7c720337bb32f84d90c8c1a543d
Konstantinos Douloudis <kdoul@unipi.gr>
2018-08-02 13:14:26 +0300

    -Drop requirements with null responses (URL, IDENTIFIER) currently missing from model

--- designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/service/RegulatedModeltoESPDResponseV2Service.java
+++ designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/service/RegulatedModeltoESPDResponseV2Service.java
@@ -3,8 +3,10 @@ package eu.esens.espdvcd.designer.service;
 import eu.esens.espdvcd.builder.BuilderFactory;
 import eu.esens.espdvcd.designer.typeEnum.ArtefactType;
 import eu.esens.espdvcd.model.ESPDResponse;
+import eu.esens.espdvcd.model.requirement.RequirementGroup;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class RegulatedModeltoESPDResponseV2Service implements ModeltoESPDService {
     private final ArtefactType artefactType;
@@ -17,6 +19,9 @@ public class RegulatedModeltoESPDResponseV2Service implements ModeltoESPDService
     public InputStream CreateXMLStreamFromModel(Object document) {
         ESPDResponse doc = (ESPDResponse) document;
         doc.getEvidenceList().removeIf(e -> e.getEvidenceURL() == null);
+        doc.getFullCriterionList().forEach(cr -> {
+            fixNullResponses(cr.getRequirementGroups());
+        });
         return BuilderFactory.withEDMVersion2().getDocumentBuilderFor((ESPDResponse) document).getAsInputStream();
     }
 
@@ -24,11 +29,24 @@ public class RegulatedModeltoESPDResponseV2Service implements ModeltoESPDService
     public String CreateXMLStringFromModel(Object document) {
         ESPDResponse doc = (ESPDResponse) document;
         doc.getEvidenceList().removeIf(e -> e.getEvidenceURL() == null);
+        doc.getFullCriterionList().forEach(cr -> {
+            fixNullResponses(cr.getRequirementGroups());
+        });
         return BuilderFactory.withEDMVersion2().getDocumentBuilderFor((ESPDResponse) document).getAsString();
     }
 
     @Override
     public ArtefactType getArtefactType() {
         return artefactType;
+    }
+
+    /*
+    FIXME: TEMPORARY FIX FOR MISSING RESPONSE DATA TYPES
+     */
+    private void fixNullResponses(List<RequirementGroup> requirementGroupList) {
+        requirementGroupList.forEach(rg ->   {
+            rg.getRequirements().removeIf(r -> r.getResponse() == null);
+            fixNullResponses(rg.getRequirementGroups());
+        });
     }
 }
