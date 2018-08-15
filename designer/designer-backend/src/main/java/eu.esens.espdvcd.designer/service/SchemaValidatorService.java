fff0e60fbeadfc5a3fcf7e89348498cc55bc4577
Konstantinos Douloudis <kdoul@unipi.gr>
2018-08-15 16:52:24 +0300

    Update validator services to work with the new ArtefactValidator API

--- designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/service/SchemaValidatorService.java
+++ designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/service/SchemaValidatorService.java
@@ -1,5 +1,6 @@
 package eu.esens.espdvcd.designer.service;
 
+import eu.esens.espdvcd.builder.util.ArtefactUtils;
 import eu.esens.espdvcd.validator.ArtefactValidator;
 import eu.esens.espdvcd.validator.Validators;
 import org.xml.sax.SAXException;
@@ -12,11 +13,11 @@ import java.io.FileNotFoundException;
 public class SchemaValidatorService implements ValidatorService {
     @Override
     public ArtefactValidator validateESPDRequest(File request) throws FileNotFoundException, JAXBException, SAXException {
-        return Validators.createESPDRequestSchemaValidator(new FileInputStream(request));
+        return Validators.createESPDRequestSchemaValidator(new FileInputStream(request), ArtefactUtils.findSchemaVersion(request));
     }
 
     @Override
     public ArtefactValidator validateESPDResponse(File response) throws FileNotFoundException, JAXBException, SAXException {
-        return Validators.createESPDResponseSchemaValidator(new FileInputStream(response));
+        return Validators.createESPDResponseSchemaValidator(new FileInputStream(response), ArtefactUtils.findSchemaVersion(response));
     }
 }
--- designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/service/SchematronValidatorService.java
+++ designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/service/SchematronValidatorService.java
@@ -1,5 +1,6 @@
 package eu.esens.espdvcd.designer.service;
 
+import eu.esens.espdvcd.builder.util.ArtefactUtils;
 import eu.esens.espdvcd.validator.ArtefactValidator;
 import eu.esens.espdvcd.validator.Validators;
 import eu.esens.espdvcd.validator.enums.SchematronOrigin;
@@ -9,11 +10,11 @@ import java.io.File;
 public class SchematronValidatorService implements ValidatorService {
     @Override
     public ArtefactValidator validateESPDRequest(File request) {
-        return Validators.createESPDRequestSchematronValidator(request, SchematronOrigin.EU);
+        return Validators.createESPDRequestSchematronValidator(request, ArtefactUtils.findSchemaVersion(request)); //,SchematronOrigin.EU);
     }
 
     @Override
     public ArtefactValidator validateESPDResponse(File response) {
-        return Validators.createESPDResponseSchematronValidator(response, SchematronOrigin.EU);
+        return Validators.createESPDResponseSchematronValidator(response, ArtefactUtils.findSchemaVersion(response));
     }
 }
