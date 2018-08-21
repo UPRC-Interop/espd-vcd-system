c723c22cc044a909fb623d60019aafcc7665106f
Maria Siapera <mariaspr@unipi.gr>
2018-08-21 14:08:32 +0300

    -mock predefined exclusion criterion

--- designer/designer-frontend/src/app/mini-render-page/mini-render-page.component.ts
+++ designer/designer-frontend/src/app/mini-render-page/mini-render-page.component.ts
@@ -25,154 +25,120 @@ export class MiniRenderPageComponent implements OnInit {
     this.exclusionACriteria = [{
       'typeCode': 'CRITERION.EXCLUSION.CONVICTIONS.PARTICIPATION_IN_CRIMINAL_ORGANISATION',
       'name': 'Participation in a criminal organisation',
-      'description': 'Has the economic operator itself or any person who is a member of its Administrative, etc...',
+      'description': 'Participation in a criminal organisation, as defined in Article 2 of Council etc...',
       'legislationReference': {
-        'title': 'DIRECTIVE 2014/24/EU OF THE EUROPEAN PARLIAMENT AND OF THE COUNCIL of 26 February 2014 etc...',
+        'title': 'DIRECTIVE 2014/24/EU OF THE EUROPEAN PARLIAMENT AND OF THE COUNCIL  etc..',
         'jurisdictionLevelCode': 'EU_DIRECTIVE',
         'article': '57(1)',
         'description': 'Directive 2014/24/EU',
         'uri': 'http://eur-lex.europa.eu/legal-content/EN/TXT/?uri=celex:32014L0024'
       },
-      'requirementGroups': [
-        {
-          'requirements': [
-            {
+      'requirementGroups': [{
+        'requirements': [{
+          'responseDataType': 'INDICATOR',
+          'description': 'Your answer',
+          'typeCode': 'QUESTION',
+          'response': null,
+          'id': '771869ed-e733-4bd0-b5ee-6ad81c50d823',
+          'uuid': '771869ed-e733-4bd0-b5ee-6ad81c50d823-5'
+        }],
+        'requirementGroups': [{
+          'requirements': [{
+            'responseDataType': 'DATE',
+            'description': 'Date of conviction',
+            'typeCode': 'QUESTION',
+            'response': null,
+            'id': '5d289637-6f8a-4a19-98cc-0e6b9e9bc8ae',
+            'uuid': '5d289637-6f8a-4a19-98cc-0e6b9e9bc8ae-5'
+          }, {
+            'responseDataType': 'DESCRIPTION',
+            'description': 'Reason',
+            'typeCode': 'QUESTION',
+            'response': null,
+            'id': '61839c98-87df-4bf1-b246-b0b86b191206',
+            'uuid': '61839c98-87df-4bf1-b246-b0b86b191206-5'
+          }, {
+            'responseDataType': 'DESCRIPTION',
+            'description': 'Who has been convicted',
+            'typeCode': 'QUESTION',
+            'response': null,
+            'id': '313603f8-4392-40ec-92bc-3a0d899de85e',
+            'uuid': '313603f8-4392-40ec-92bc-3a0d899de85e-5'
+          }, {
+            'responseDataType': 'DESCRIPTION',
+            'description': 'Length of the period of exclusion',
+            'typeCode': 'QUESTION',
+            'response': null,
+            'id': 'e1e5308f-f67a-4e52-8723-a5e2b19fe0a8',
+            'uuid': 'e1e5308f-f67a-4e52-8723-a5e2b19fe0a8-5'
+          }],
+          'requirementGroups': [{
+            'requirements': [{
               'responseDataType': 'INDICATOR',
-              'description': 'Your answer?',
+              'description': 'Have you taken measures to demonstrate your reliability (Self-Cleaning)?',
               'typeCode': 'QUESTION',
-              'response': {
-                'validatedCriterionPropertyID': '20e90057-43a6-42a6-9769-d3d6222a60e0',
-                'confidentialityLevelCode': 'PUBLIC',
-                'responseType': 'INDICATOR',
-                'indicator': false,
-                'id': null,
-                'uuid': null
-              },
-              'id': '20e90057-43a6-42a6-9769-d3d6222a60e0',
-              'uuid': '20e90057-43a6-42a6-9769-d3d6222a60e0-5'
-            }
-          ],
-          'requirementGroups': [
-            {
-              'requirements': [
-                {
-                  'responseDataType': 'DATE',
-                  'description': 'Date of conviction',
-                  'typeCode': 'QUESTION',
-                  'response': null,
-                  'id': '4760ea36-4480-4440-b1e3-f0baa9106d7c',
-                  'uuid': '4760ea36-4480-4440-b1e3-f0baa9106d7c-5'
-                },
-                {
-                  'responseDataType': 'DESCRIPTION',
-                  'description': 'Reason',
-                  'typeCode': 'QUESTION',
-                  'response': null,
-                  'id': '7f8f9fb0-d405-42ca-a270-6b58d9cde696',
-                  'uuid': '7f8f9fb0-d405-42ca-a270-6b58d9cde696-5'
-                },
-                {
-                  'responseDataType': 'DESCRIPTION',
-                  'description': 'Who has been convicted',
-                  'typeCode': 'QUESTION',
-                  'response': null,
-                  'id': '3b7462a7-5aaa-4f65-96f0-5a7ffe5cb8e3',
-                  'uuid': '3b7462a7-5aaa-4f65-96f0-5a7ffe5cb8e3-5'
-                },
-                {
-                  'responseDataType': 'DESCRIPTION',
-                  'description': 'Length of the period of exclusion',
-                  'typeCode': 'QUESTION',
-                  'response': null,
-                  'id': '655cc87c-48a3-4921-be67-9a6b7e8076ac',
-                  'uuid': '655cc87c-48a3-4921-be67-9a6b7e8076ac-5'
-                }
-              ],
-              'requirementGroups': [
-                {
-                  'requirements': [
-                    {
-                      'responseDataType': 'INDICATOR',
-                      'description': 'Have you taken measures to demonstrate your reliability ("Self-Cleaning")?',
-                      'typeCode': 'QUESTION',
-                      'response': null,
-                      'id': 'fb833952-b629-4577-9a8a-7b429b98f943',
-                      'uuid': 'fb833952-b629-4577-9a8a-7b429b98f943-5'
-                    }
-                  ],
-                  'requirementGroups': [
-                    {
-                      'requirements': [
-                        {
-                          'responseDataType': 'DESCRIPTION',
-                          'description': 'Please describe them',
-                          'typeCode': 'QUESTION',
-                          'response': null,
-                          'id': '1610fb0a-74d6-4527-be99-97664fdfd159',
-                          'uuid': '1610fb0a-74d6-4527-be99-97664fdfd159-5'
-                        }
-                      ],
-                      'requirementGroups': [],
-                      'condition': 'ONTRUE',
-                      'id': '74e6c7b4-757b-4b40-ada6-fad6a997c310',
-                      'uuid': '74e6c7b4-757b-4b40-ada6-fad6a997c310-4'
-                    }
-                  ],
-                  'condition': 'ONTRUE',
-                  'id': 'f4978772-3126-4ded-bc30-f50da8c3a038',
-                  'uuid': 'f4978772-3126-4ded-bc30-f50da8c3a038-3'
-                }
-              ],
-              'condition': 'ONTRUE',
-              'id': 'f5276600-a2b6-4ff6-a90e-b31fe19dae41',
-              'uuid': 'f5276600-a2b6-4ff6-a90e-b31fe19dae41-2'
-            }
-          ],
-          'condition': 'ON*',
-          'id': '7c637c0c-7703-4389-ba52-02997a055bd7',
-          'uuid': '7c637c0c-7703-4389-ba52-02997a055bd7-1'
-        },
-        {
-          'requirements': [
-            {
-              'responseDataType': 'INDICATOR',
-              'description': 'Is this information available electronically?',
-              'typeCode': 'QUESTION',
-              'response': {
-                'validatedCriterionPropertyID': '4bada04a-933d-436b-8f46-70c8c07ac90c',
-                'confidentialityLevelCode': 'PUBLIC',
-                'responseType': 'INDICATOR',
-                'indicator': false,
-                'id': null,
-                'uuid': null
-              },
-              'id': '4bada04a-933d-436b-8f46-70c8c07ac90c',
-              'uuid': '4bada04a-933d-436b-8f46-70c8c07ac90c-7'
-            }
-          ],
-          'requirementGroups': [
-            {
-              'requirements': [
-                {
-                  'responseDataType': 'EVIDENCE_IDENTIFIER',
-                  'description': 'Evidence supplied',
-                  'typeCode': 'QUESTION',
-                  'response': null,
-                  'id': 'b618722d-7661-4770-bc03-f0e5bb43393b',
-                  'uuid': 'b618722d-7661-4770-bc03-f0e5bb43393b-7'
-                }
-              ],
+              'response': null,
+              'id': '397f7209-2efb-414a-b008-3f3c527a09fc',
+              'uuid': '397f7209-2efb-414a-b008-3f3c527a09fc-5'
+            }],
+            'requirementGroups': [{
+              'requirements': [{
+                'responseDataType': 'DESCRIPTION',
+                'description': 'Please describe them',
+                'typeCode': 'QUESTION',
+                'response': null,
+                'id': '244aa3ab-ae24-4c56-8f3a-6626074de931',
+                'uuid': '244aa3ab-ae24-4c56-8f3a-6626074de931-5'
+              }],
               'requirementGroups': [],
+              'ruleSet': null,
               'condition': 'ONTRUE',
-              'id': '41dd2e9b-1bfd-44c7-93ee-56bd74a4334b',
-              'uuid': '41dd2e9b-1bfd-44c7-93ee-56bd74a4334b-6'
-            }
-          ],
-          'condition': 'ON*',
-          'id': '7458d42a-e581-4640-9283-34ceb3ad4345',
-          'uuid': '7458d42a-e581-4640-9283-34ceb3ad4345-5'
-        }
-      ],
+              'id': '74e6c7b4-757b-4b40-ada6-fad6a997c310',
+              'uuid': '74e6c7b4-757b-4b40-ada6-fad6a997c310-4'
+            }],
+            'ruleSet': null,
+            'condition': 'ONTRUE',
+            'id': 'f4978772-3126-4ded-bc30-f50da8c3a038',
+            'uuid': 'f4978772-3126-4ded-bc30-f50da8c3a038-3'
+          }],
+          'ruleSet': null,
+          'condition': 'ONTRUE',
+          'id': 'f5276600-a2b6-4ff6-a90e-b31fe19dae41',
+          'uuid': 'f5276600-a2b6-4ff6-a90e-b31fe19dae41-2'
+        }],
+        'ruleSet': null,
+        'condition': 'ON*',
+        'id': '7c637c0c-7703-4389-ba52-02997a055bd7',
+        'uuid': '7c637c0c-7703-4389-ba52-02997a055bd7-1'
+      }, {
+        'requirements': [{
+          'responseDataType': 'INDICATOR',
+          'description': 'Is this information available electronically?',
+          'typeCode': 'QUESTION',
+          'response': null,
+          'id': 'be3753e3-9273-4a68-9ad2-c19886d47576',
+          'uuid': 'be3753e3-9273-4a68-9ad2-c19886d47576-7'
+        }],
+        'requirementGroups': [{
+          'requirements': [{
+            'responseDataType': 'EVIDENCE_IDENTIFIER',
+            'description': 'Evidence supplied',
+            'typeCode': 'QUESTION',
+            'response': null,
+            'id': 'ca0f12ca-f75d-42f0-92e9-c5c129c21b25',
+            'uuid': 'ca0f12ca-f75d-42f0-92e9-c5c129c21b25-7'
+          }],
+          'requirementGroups': [],
+          'ruleSet': null,
+          'condition': 'ONTRUE',
+          'id': '41dd2e9b-1bfd-44c7-93ee-56bd74a4334b',
+          'uuid': '41dd2e9b-1bfd-44c7-93ee-56bd74a4334b-6'
+        }],
+        'ruleSet': null,
+        'condition': 'ON*',
+        'id': '7458d42a-e581-4640-9283-34ceb3ad4345',
+        'uuid': '7458d42a-e581-4640-9283-34ceb3ad4345-5'
+      }],
       'selected': true,
       'id': '005eb9ed-1347-4ca3-bb29-9bc0db64e1ab',
       'uuid': '005eb9ed-1347-4ca3-bb29-9bc0db64e1ab',
@@ -197,6 +163,7 @@ export class MiniRenderPageComponent implements OnInit {
   onMockSubmit() {
     const formValues = this.exclusionACriteriaForm.getRawValue();
     console.log(formValues);
+    console.log(this.exclusionACriteriaForm);
   }
 
 }
