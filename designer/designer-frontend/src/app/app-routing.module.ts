58df02f9a121b222a792493c25de3f9b108d948f
Maria Siapera <mariaspr@unipi.gr>
2018-08-31 15:14:42 +0300

    -v2 request satisfiesALL render fix

--- designer/designer-frontend/src/app/app-routing.module.ts
+++ designer/designer-frontend/src/app/app-routing.module.ts
@@ -1,18 +1,13 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {RootComponent} from './root/root.component';
-import {MiniRenderPageComponent} from './mini-render-page/mini-render-page.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [],
     component: RootComponent
-  }, {
-    path: 'minipage',
-    children: [],
-    component: MiniRenderPageComponent
-  },
+  }
 ];
 
 @NgModule({
