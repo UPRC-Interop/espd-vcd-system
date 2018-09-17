731bba900c04f24a826e139c715105156e249a6c
Maria Siapera <mariaspr@unipi.gr>
2018-09-17 13:37:46 +0300

    -Added ngx-translate module -Added property files in asset folder

--- designer/designer-frontend/src/app/app.component.ts
+++ designer/designer-frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {DataService} from './services/data.service';
+import {TranslateService} from '@ngx-translate/core';
 
 // import {NgForm, FormControl} from "@angular/forms";
 
@@ -11,6 +12,12 @@ import {DataService} from './services/data.service';
 export class AppComponent {
   isLinear = true;
 
-  constructor(public dataService: DataService) {
+  constructor(public dataService: DataService, public translate: TranslateService) {
+    translate.setDefaultLang('ESPD_en');
+    // translate.setDefaultLang('ESPD_de');
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
   }
 }
