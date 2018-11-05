92a08c5879d91e9709bb40468b1a15403adc3e45
frenzel <Daniel.Frenzel@adesso.de>
2018-11-05 09:16:16 +0100

    DatePicker meldet keine Fehler wenn das Feld Blank ist

--- designer/designer-frontend/src/app/directives/validation-date-fix/date-validation-fix.directive.ts
+++ designer/designer-frontend/src/app/directives/validation-date-fix/date-validation-fix.directive.ts
@@ -1,5 +1,6 @@
-import {Directive, HostListener, Input} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
 import {AbstractControl} from "@angular/forms";
+import {StringHelperService} from "../../services/string-helper.service";
 
 /**
  * This directive is a workaround for this angular material bug: https://github.com/angular/material2/issues/13346
@@ -12,9 +13,19 @@ export class DateValidationFixDirective {
 
   @Input('appDateValidationFix') formControl: AbstractControl;
 
+  constructor(private elementRef: ElementRef) {
+  }
+
   @HostListener('blur') onFocusLost() {
-    if (this.formControl.value === null || this.formControl.value === "") {
+    let nativeElementValue = this.elementRef.nativeElement.value;
+    if (this.formControl.value === null && !StringHelperService.isBlank(nativeElementValue)) {
       this.formControl.setErrors({'matDatepickerParse': true});
+    }
+    else {
+      if (this.formControl.hasError('matDatepickerParse')) {
+        delete this.formControl.errors['matDatepickerParse'];
+        this.formControl.updateValueAndValidity();
+      }
     }
   }
 }
