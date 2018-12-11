c00e0af9688a6d6eae00edab6ef65881ad6fd590
Alex <alkla2715@gmail.com>
2018-12-11 08:38:43 +0100

    fixed - wizard validation on espd-review

--- designer/designer-frontend/src/app/services/validation.service.ts
+++ designer/designer-frontend/src/app/services/validation.service.ts
@@ -1,13 +1,18 @@
 import {Injectable} from '@angular/core';
+import {UtilitiesService} from "./utilities.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidationService {
 
-  constructor() { }
+  constructor(private utilitiesService: UtilitiesService) { }
 
   public validateFormsInComponent(forms): boolean {
+    if (this.utilitiesService.isReviewESPD) {
+      return true;
+    }
+
     let valid: boolean = true;
       forms.forEach(ngForm => {
         if (ngForm.form !== null && !ngForm.form.valid) {
