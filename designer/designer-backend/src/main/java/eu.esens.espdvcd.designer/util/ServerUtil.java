02b3b08d504baada794a1ffcdb72387d948b6570
Konstantinos Douloudis <kdoul@unipi.gr>
2018-12-21 17:38:26 +0200

    - Fix video frame that wouldn't load

--- designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/util/ServerUtil.java
+++ designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/util/ServerUtil.java
@@ -83,7 +83,7 @@ public final class ServerUtil {
     private static void initHeaderMap(){
         securityHeaders = new HashMap<>();
 
-        String contentSecurityPolicyHeaders = "default-src 'self' http://83.212.109.183/ https://europa.eu/!nY89JQ; " +
+        String contentSecurityPolicyHeaders = "default-src 'self' http://83.212.109.183/ https://europa.eu/ http://ec.europa.eu/; " +
                 "font-src https://fonts.googleapis.com https://fonts.gstatic.com; " +
                 "img-src 'self' ;" +
                 "object-src 'none'; " +
