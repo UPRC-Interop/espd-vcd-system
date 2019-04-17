0b4f90a930f44e2e82c9ce4dfde831c44e22f819
Konstantinos Douloudis <kdoul@unipi.gr>
2019-04-17 12:45:25 +0300

    - Attempt to fix video loading

--- designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/util/ServerUtil.java
+++ designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/util/ServerUtil.java
@@ -98,7 +98,7 @@ public final class ServerUtil {
     }
 
     /**
-     * Lazy initialization for the header HashMap
+     * Lazy initialization for the header Map
      */
     private static void initHeaderMap() {
         securityHeaders = new HashMap<>();
@@ -106,7 +106,7 @@ public final class ServerUtil {
         String contentSecurityPolicyHeaders = "default-src 'none'; " +
                 "font-src https://fonts.gstatic.com; " +
                 "img-src 'self'; " +
-                "child-src 'http://audiovisual.ec.europa.eu'; " +
+                "child-src 'http://audiovisual.ec.europa.eu/avservices/play.cfm?ref=I125339&lg=EN/EN&sublg=none&autoplay=false'; " +
                 "object-src 'none'; " +
                 "script-src 'self' ; " +
                 "connect-src 'self'; " +
