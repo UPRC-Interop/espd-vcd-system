5eb6dc3219c9e9764f6ad39a5708b27334af9b9d
Konstantinos Douloudis <kdoul@unipi.gr>
2019-04-23 11:15:48 +0300

    - Https fixes  - Header fixes

--- designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/util/ServerUtil.java
+++ designer/designer-backend/src/main/java/eu.esens.espdvcd.designer/util/ServerUtil.java
@@ -106,7 +106,7 @@ public final class ServerUtil {
         String contentSecurityPolicyHeaders = "default-src 'none'; " +
                 "font-src https://fonts.gstatic.com; " +
                 "img-src 'self'; " +
-                "child-src 'http://audiovisual.ec.europa.eu'; " +
+                "child-src 'https://audiovisual.ec.europa.eu'; " +
                 "object-src 'none'; " +
                 "script-src 'self' ; " +
                 "connect-src 'self'; " +
